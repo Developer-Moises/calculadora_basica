399cf8dfd0d3bd1551fa94e2bb0993db34c54565
Developer-Moises <mc02052005@gmail.com>
2025-11-10 13:35:04 -0500

    Segunda versión: se cambiaron los mensajes de las operaciones

--- calculadora.py
+++ calculadora.py
@@ -4,14 +4,14 @@ numero_1 = float(input("Primer número: "))
 numero_2 = float(input("Segundo número: "))
 operacion = input("Operación (+, -, *, /): ")
 if operacion == '+':
-    print("Resultado:", numero_1 + numero_2)
+    print(f"Resultado de la suma de {numero_1} + {numero_2}:", numero_1 + numero_2)
 elif operacion == '-':
-    print("Resultado:", numero_1 - numero_2)
+    print(f"Resultado de la resta de {numero_1} - {numero_2}:", numero_1 - numero_2)
 elif operacion == '*':
-    print("Resultado:", numero_1 * numero_2)
+    print(f"Resultado de la multiplicación de {numero_1} * {numero_2}:", numero_1 * numero_2)
 elif operacion == '/':
     if numero_2 != 0:
-        print("Resultado:", numero_1 / numero_2)
+        print(f"Resultado de la división de {numero_1} / {numero_2}:", numero_1 / numero_2)
     else:
         print("Error: no se puede dividir por cero.")
 else:
